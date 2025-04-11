ae60cd8018b47c68ccb49fe66f38570a1c8affce
Mahlstrom <magnus@atuin.se>
2025-04-11 20:57:42 +0200

    first

---
+++ setup.sh
@@ -0,0 +1,23 @@
+function checksymlink() {
+    local path="$1"
+    local target="$2"
+    if [ "$(readlink -- "$path")" = "$target" ]; then
+        echo "$path is correctly symlinked"
+    else
+        echo "$path is not correct. Now: \"$(readlink -- "$path")\" Wanted: \"$target\""
+    fi
+}
+function check_install(){
+    thecmd="$1"
+    the_installcmd="$2"
+    if ! command -v $thecmd 2>&1 >/dev/null
+    then
+        echo "installing $thecmd"
+        echo "$the_installcmd"
+    fi
+}
+check_install "git" "xcode-select --install"
+check_install "brew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null ## /dev/null skips pressing enter for the installation'
+
+checksymlink "$HOME/.config/nvim_files" "../github.com"
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
